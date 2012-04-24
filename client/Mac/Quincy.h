de990b141834991fa5eb216e80e6bb355bbfed71
Damien DeVille <d.deville@me.com>
2012-04-24 13:10:12 -0700

    Make a simple project file that build the framework. This way, we do not need to add the Demo Project as a subproject in order to build the framework !?
    Also, provide a root header to simply being able to #import <Quincy/Quincy.h>
    
    Framework 101 really...

---
+++ client/Mac/Quincy.h
@@ -0,0 +1,11 @@
+//
+//  Quincy.h
+//  Quincy
+//
+//  Created by Damien DeVille on 24/04/2012.
+//  Copyright (c) 2012 Realmac Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "Quincy/BWQuincyManager.h"
---
+++ client/Mac/Quincy.m
@@ -0,0 +1,10 @@
+//
+//  Quincy.m
+//  Quincy
+//
+//  Created by Damien DeVille on 24/04/2012.
+//  Copyright (c) 2012 Realmac Software. All rights reserved.
+//
+
+#import "Quincy.h"
+
